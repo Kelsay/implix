1b684269aef8304bcd971469eba9df59cd13a612
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2017-07-05 02:45:48 +0200

    Updated base pipeline

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+﻿var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './source/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: './source/index.html',
+            to: __dirname + '/dist/index.html'
+        }])
+    ],
+    devServer: {
+        contentBase: './dist'
+    }
+};
