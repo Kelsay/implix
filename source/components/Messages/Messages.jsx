1eb9254f05caea92266da20dc45b4751ac3760ba
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2017-07-09 02:19:56 +0200

    Messages are now working

--- source/components/Messages/Messages.jsx
+++ source/components/Messages/Messages.jsx
@@ -6,24 +6,48 @@ export default class Messages extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            messages: []
+            messages: {}
         };
+        this.counter = 0;
+        this.timeouts = [];
         let receiver = PubSub.subscribe('MessagesUpdate', this.updateReceiver.bind(this));
     }
 
     updateReceiver(msg, data) {
-        this.setState({ 'messages': this.state.messages.concat(data)});
+        let messages = {};
+        for (let message of data) {
+            messages[`message_${this.counter += 1}`] = message;
+        }
+        this.setState({'messages': Object.assign(this.state.messages, messages)});
+    }
+
+    removeMessage(id) {
+        let newMessages = Object.assign({}, this.state.messages);
+        newMessages[id] = null;
+        this.setState({messages: newMessages});
+    }
+
+    hideMessageBox(id) {
+        console.log(this.ref[id]);
     }
 
     render() {
+        let messages = [];
+        for (let id in this.state.messages) {
+            let text = this.state.messages[id];
+            if (text !== null) {
+                messages.push(<li ref={id} key={id} className="message">{text}</li>);
+                if (!this.timeouts[id]) {
+                    this.timeouts[id] = setTimeout(this.removeMessage.bind(this, id), 3000);
+                }
+            }
+        }
 
-        let messages = this.state.messages.map(function (text, index) {
-            return <li key={index}>{text}</li>;
-        });
-        
         return (
             <div className="messages">
-                {messages}
+                <ul>
+                    {messages}
+                </ul>
             </div>
         );
     }
